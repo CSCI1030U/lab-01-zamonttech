2b5a65e8d9a5a292c69787b2be81e7e5ef1c360d
zamonttech <zamuel.bautista@ontariotechu.net>
2025-09-10 15:22:31 -0400

    Lab 01 completed code

--- lab01.py
+++ lab01.py
@@ -3,23 +3,29 @@ def main():
     quantity = 5 
 
     # YOUR CODE FOR PART 1 GOES HERE  
-
+    subtotal_cost = quantity * cost_per_item
+    tax = subtotal_cost * 0.13
+    total_cost = subtotal_cost + tax
 
     # YOUR CODE FOR PART 2 GOES HERE
     print(f'cost_per_item = ${cost_per_item:0.2f}') # a sample for you to use for the other prices
+    print(f"quantity = {quantity}")
+    print(f'subtotal_cost = ${subtotal_cost:0.2f}')
+    print(f'tax = ${tax:0.2f}')
+    print(f'total_cost = ${total_cost:0.2f}')
 
 
     # THIS IS THE CODE FOR PART 3
-    # initial_investment = 1000
-    # interest_rate = 0.035
-    # investment = initial_investment
-    # investment += investment * interest_rate
-    # investment += investment * interest_rate
-    # investment += investment * interest_rate
-    # investment += investment * interest_rate
-    # investment += investment * interest_rate
-    # print('After 5 years, your investment will be worth ' + investment + ' dollars.')
-    # expected output: After 5 years, your investment will be worth 1187.6863056468749 dollars.
+    initial_investment = 1000
+    interest_rate = 0.035
+    investment = initial_investment
+    investment += investment * interest_rate
+    investment += investment * interest_rate
+    investment += investment * interest_rate
+    investment += investment * interest_rate
+    investment += investment * interest_rate
+    print(f'After 5 years, your investment will be worth {investment} dollars.')
+    #expected output: After 5 years, your investment will be worth 1187.6863056468749 dollars.
 
 if __name__ == "__main__":
     main()
